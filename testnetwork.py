9a12c39c555d6bf87f56b834189f85dddf9b39f6
Simon <simon.granstrom@gmail.com>
2020-06-28 21:09:16 +0200

    tried to do something, not finished but idk if I will

---
+++ testnetwork.py
@@ -0,0 +1,217 @@
+# Preprocessing
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+import json
+
+# Net
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+REBUILD_DATA = False
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on GPU")
+else:
+    device = torch.device("cpu")
+    print("Running on CPU")
+
+class DogsVSCats():
+    IMG_SIZE = 50
+    CATS = "data/Cat"
+    DOGS = "data/Dog"
+    LABELS = {CATS: 0, DOGS: 1}
+    training_data = []
+    catcount = 0
+    dogcount = 0
+
+    def make_training_data(self):
+        for label in self.LABELS:
+            print(label)
+            for f in tqdm(os.listdir(label)):
+                try:
+                    path = os.path.join(label, f)
+                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                    self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])
+
+                    if label == self.DOGS:
+                        self.dogcount += 1
+                    elif label == self.CATS:
+                        self.catcount += 1
+                except Exception as e:
+                    pass
+
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
+        print("Cats: ", self.catcount)
+        print("Dogs: ", self.dogcount)
+
+class Football():
+    teamsResultChain = {}
+    minChainLength = 60
+    chainStep = 15
+
+    train_data = []
+
+    def load_data(self):
+        f = open("premierleague.json")
+        _j = json.loads(f.read())
+        f.close()
+        for j in _j:
+            for fix in j['fixtures']:
+                # result : 1 = home, -1 away, 0 draw
+                if fix['goalsHomeTeam'] is None or fix['goalsAwayTeam'] is None:
+                    continue
+                
+                result = fix['goalsHomeTeam'] > fix['goalsAwayTeam']
+                if fix['goalsHomeTeam'] == fix['goalsAwayTeam']:
+                    result = 0
+
+                ht = fix['homeTeam']['team_name']
+                at = fix['awayTeam']['team_name']
+                
+                if ht in self.teamsResultChain:
+                    self.teamsResultChain[ht].append('W' * result + 'L' * -result + 'D' * 0**result**2)
+                else:
+                    self.teamsResultChain[ht] = ['W' * result + 'L' * -result + 'D' * 0**result**2,]
+
+                if at in self.teamsResultChain:
+                    self.teamsResultChain[at].append('W' * -result + 'L' * result + 'D' * 0**result**2)
+                else:
+                    self.teamsResultChain[at] = ['W' * -result + 'L' * result + 'D' * 0**result**2,]
+
+        remove = []
+        for chain in self.teamsResultChain:
+            if (len(self.teamsResultChain[chain]) < self.minChainLength):
+                remove.append(chain)
+
+        for rem in remove:
+            self.teamsResultChain.pop(rem)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(15, 15).view(-1,1,15,15)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
+
+    def convs(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+
+        return x
+
+    def forward(self, x): 
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return F.softmax(x, dim=1)
+
+
+def train(net):
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+    for epoch in range(EPOCHS):
+        for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+            batch_X = train_X[i:i+BATCH_SIZE].view(-1,1,15,15).to(device)
+            batch_y = train_y[i:i+BATCH_SIZE].to(device)
+
+            net.zero_grad()
+            outputs = net(batch_X)
+            loss = loss_function(outputs, batch_y)
+            loss.backward()
+            optimizer.step()
+
+    print(f"Epoch: {epoch}. Loss {loss}")
+
+def test(net):
+    correct = 0
+    total = 0
+    # Evaluate the model/net
+    with torch.no_grad():
+        for i in tqdm(range(len(test_X))):
+            real_class = torch.argmax(test_y[i]).to(device)
+            net_out = net(test_X[i].view(-1,1,15,15).to(device))[0]
+            predicted_class = torch.argmax(net_out)
+            if predicted_class == real_class:
+                correct += 1
+            total += 1
+
+    print("Accuracy: ", round(correct/total, 3))
+
+
+if REBUILD_DATA:
+    dogsvcats = DogsVSCats()
+    dogsvcats.make_training_data()
+
+print("Loading data")
+football = Football()
+football.load_data()
+training_data = 
+
+net = Net().to(device)
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1, 15, 15)
+y = torch.tensor([i[1] for i in training_data])
+
+VAL_PCT = 0.1
+val_size = int(len(X)*VAL_PCT)
+print(val_size, " .")
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+
+BATCH_SIZE = 100
+EPOCHS = 20
+
+train(net)
+test(net)
+
+"""
+training_data = np.load("training_data.npy", allow_pickle=True)
+
+net = Net().to(device)
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
+X = X/255.0
+y = torch.Tensor([i[1] for i in training_data])
+
+VAL_PCT = 0.1
+val_size = int(len(X)*VAL_PCT)
+print(val_size)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+
+BATCH_SIZE = 100
+EPOCHS = 1
+
+train(net)
+test(net)
+
+"""
